7a65e114907d1295d0fb1a1e660d94e91ea29d7f
ValentinDePascale <valentindp9@gmail.com>
2026-08-07 19:31:38 -0300

    chore: remove unused imports from ofertas page

--- greatphones-next/src/app/ofertas/page.tsx
+++ greatphones-next/src/app/ofertas/page.tsx
@@ -1,5 +1,2 @@
-import { readFileSync, existsSync } from 'fs'
-import { join } from 'path'
 import { serveSpa } from '@/lib/spa-pages'
-
 export default function Page() { return <div dangerouslySetInnerHTML={{ __html: serveSpa('ofertas') }} suppressHydrationWarning /> }
